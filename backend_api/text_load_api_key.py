b2b7fc4312b0d272a4dae9ca40a8dd7aae5d4ace
gsli-git <gsli_wsu@yahoo.com>
2025-01-12 21:13:13 -0500

    update code showing part of key.

--- backend_api/text_load_api_key.py
+++ backend_api/text_load_api_key.py
@@ -7,4 +7,4 @@ load_dotenv()
 # Access the OpenAI API key
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-print(f"New OpenAI API key is: {openai_api_key[:15]}")
+print(f"New OpenAI API key is: {openai_api_key[:25]}")
