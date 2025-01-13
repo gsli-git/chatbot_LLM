a819277b3fb532ea2f89164030605976731d36d5
gsli-git <gsli_wsu@yahoo.com>
2025-01-12 19:42:07 -0500

    updated files on handling API_key

---
+++ backend_api/text_load_api_key.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+# Access the OpenAI API key
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+print(f"New OpenAI API key is: {openai_api_key}")
